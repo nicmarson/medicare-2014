b2924ba9b8c98e625fbaf1c41502c382c0d32282
Tara <mstaragrieb@gmail.com>
2013-01-25 12:38:33 -0800

    More

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+
+$(function() {
+$('#myModal').modal('show');
+});
+
+
 // Our Javascript Calls
 
 // $(function() {
@@ -84,66 +90,66 @@
 // 
 // });
 
-angular.module('medicare', ['ui.bootstrap']);
-function AccordionDemoCtrl($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-	{
-	  title: "Dynamic Group Header - 1",
-	  content: "Dynamic Group Body - 1"
-	},
-	{
-	  title: "Dynamic Group Header - 2",
-	  content: "Dynamic Group Body - 2"
-	}
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-	$scope.items.push('Item ' + $scope.items.length);
-  };
-}
-
-
-function FormDialog($scope, $dialog){
-
-  // Inlined template for demo
-  var t = '<div class="modal-header">'+
-          '<h1>This is the title</h1>'+
-          '</div>'+
-          '<div class="modal-body">'+
-          '<p>Enter a value to pass to <code>close</code> as the result: <input ng-model="result" /></p>'+
-          '</div>'+
-          '<div class="modal-footer">'+
-          '<button ng-click="close(result)" class="btn btn-primary" >Close</button>'+
-          '</div>';
-   //console.log($scope);
-  $scope.opts = {
-    backdrop: true,
-    keyboard: true,
-    backdropClick: true,
-    template:  t, // OR: templateUrl: 'path/to/view.html',
-    controller: 'TestDialogController'
-  };
-
-  $scope.openDialog = function(e){
-  	e.preventDefault();
-  	console.log(e)
-  	var d = $dialog.dialog($scope.opts);
-    d.open().then(function(result){
-      if(result)
-      {
-        alert('dialog closed with result: ' + result);
-      }
-    });
-  };
-}
-
-// the dialog is injected in the specified controller
-function TestDialogController($scope, dialog){
-  $scope.close = function(result){
-    dialog.close(result);
-  };
-}
+// angular.module('medicare', ['ui.bootstrap']);
+// function AccordionDemoCtrl($scope) {
+//   $scope.oneAtATime = true;
+// 
+//   $scope.groups = [
+// 	{
+// 	  title: "Dynamic Group Header - 1",
+// 	  content: "Dynamic Group Body - 1"
+// 	},
+// 	{
+// 	  title: "Dynamic Group Header - 2",
+// 	  content: "Dynamic Group Body - 2"
+// 	}
+//   ];
+// 
+//   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+// 
+//   $scope.addItem = function() {
+// 	$scope.items.push('Item ' + $scope.items.length);
+//   };
+// }
+// 
+// 
+// function FormDialog($scope, $dialog){
+// 
+//   // Inlined template for demo
+//   var t = '<div class="modal-header">'+
+//           '<h1>This is the title</h1>'+
+//           '</div>'+
+//           '<div class="modal-body">'+
+//           '<p>Enter a value to pass to <code>close</code> as the result: <input ng-model="result" /></p>'+
+//           '</div>'+
+//           '<div class="modal-footer">'+
+//           '<button ng-click="close(result)" class="btn btn-primary" >Close</button>'+
+//           '</div>';
+//    //console.log($scope);
+//   $scope.opts = {
+//     backdrop: true,
+//     keyboard: true,
+//     backdropClick: true,
+//     template:  t, // OR: templateUrl: 'path/to/view.html',
+//     controller: 'TestDialogController'
+//   };
+// 
+//   $scope.openDialog = function(e){
+//   	e.preventDefault();
+//   	console.log(e)
+//   	var d = $dialog.dialog($scope.opts);
+//     d.open().then(function(result){
+//       if(result)
+//       {
+//         alert('dialog closed with result: ' + result);
+//       }
+//     });
+//   };
+// }
+// 
+// // the dialog is injected in the specified controller
+// function TestDialogController($scope, dialog){
+//   $scope.close = function(result){
+//     dialog.close(result);
+//   };
+// }
