13284e72ebc64e28a6b6454ffc523dbc84bbadef
Tara <mstaragrieb@gmail.com>
2013-01-30 15:44:52 -0800

    Current

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(function() {
 	displayInitialModal(demographicsDataModel);
 	
 	// Event Binding Setup for Demographics Form, will call other functions
-	attachDemographicsForm();
+	attachDemographicsForm(demographicsDataModel);
 	
 	// Event Binding Setup for External Site Links.
 	attachExternalSite();
@@ -180,17 +180,338 @@ function attachExternalSite(){
 
 /***********
 *	@name:			attachDemographicsForm()
-*	@description:	Attach Events and Listeners for the Demographics Form
+*	@description:	Attach Events and Listeners for the Demographics Form. Contains Legacy functionality
 *
 *	@param: 		N/A
 *	@returns: 		N/A
+*
+*	@TODO			Remove and refactor legacy stuff as time allows.
 ***********/
-function attachDemographicsForm(){
+function attachDemographicsForm(dataModel){
+	// On Visible Focus on the first. ??? Needs Testing
+	$('#zipCode:visible').focus();
+	
+	// Set inputs with existing values if set int the data model.
+	setFormValues(dataModel);
+	
+	// There has to be a better way to do this.
+	$("#zipCode").blur(function () {
+		handleZip();
+	});
+	$("#zipCode").keyup(function () {
+		handleZip();
+	});
+
+	function handleZip() {
+		//alert('ding');
+		var zipCookie = getCookie('ZIP');
+		var zipValue = $("#zipCode").val();
+		if ( zipValue.length === 5 ){
+			// Get the plan value from planCodeService.do
+			// Returns xml w/ plancode & area
+			var zipcode = zipValue;
+			$.ajax({
+				
+				type: "GET",
+				//url: "/planCodeService.do",
+				url: "http://regence.com/planCodeService.do",
+				data: "zip=" + $("#zipCode").val(),
+				//async: false,
+				statusCode: {
+					404: function() {
+					  validatePlanValue("", "SYSTEM_ERROR");
+					}
+				},
+				success: function (xmlDoc) {
+				  $(xmlDoc).find("plancode").each(function(){
+					var planValue = $(this).find('value').text();
+					var area    = $(this).find('area').text();
+					var errorValue  = $(this).find('error').text();
+					var age     = $("#age").val();
+
+					// TEMP FIX: Asotin county medicare products are provided by ID and not ASURIS 
+					// on the individual application Asotin is provided by ASURIS
+					// Asotin ZIP codes are 99401, 99402, 99403
+					var z = $("#zipCode").val();
+					if ( z == "99401" || z == "99402" || z == "99403" ) {
+					  planValue = 'WARBS';
+					  area = 'id';
+					}
+
+					// if (errorValue == "COUNTY_CHALLENGE") {
+					//  planValue = "OR";
+					//  errorValue = "";
+					// }
+
+					$("#planvalue").val(planValue); // Set planValue
+
+					validatePlanValue(planValue, errorValue);
+					if ( formValidated == false) {
+					  return false;
+					}
+
+					// Set state value
+					if (area == 'clark_co'){
+					  $("#state").val('WA');
+					} else {
+					  $("#state").val(planValue);
+					}
+
+					// hide the county selection
+					if (planValue != 'ASURIS' && planValue != 'ERROR'){
+					  var counties = [];
+					  // Clark County returns OR although we want the county list from WARBS
+					  if (area == 'clark_co'){planValue = 'WARBS'; if(debug){alert('returning clark county');}} 
+					  counties = $(this).getCountyList(planValue);
+					  var send_data = { 'collection':counties };
+					  createCountyList(send_data);
+  
+					  // modify ZIP cookie
+					  //$.cookie('ZIP', ( zipcode ), {path: '/', expires: 0});
+					  setCookie('ZIP', ( zipcode ), 0);
+					  //$.cookie('PLANCODE', ( planValue ), {path: '/', expires: 0});
+  					  setCookie('PLANCODE', ( planValue ), 0);
+  
+					  if (debug){
+						var cookieZip = $.cookie("ZIP");
+						var cookiePlancode = $.cookie("PLANCODE");
+						if(debug){alert('ZIP cookie: \n'+cookieZip+'\n PLANCODE cookie: \n'+cookiePlancode);}
+					  }
+					}
+				  });
+				}
+			  });
+			  
+			  
+		}
+	}
+	
 	$('#demographicSaveButton').click(function(){
 		$('#getQuoteForm').submit();
 	});
 	
 }
+
+// Added Function - setColorBox
+// Passes the attributes from desired element properties into a new colorbox
+// button: button name
+// width: width of colorbox
+// height: height of colorbox
+// action: action being assigned to the form on the colorbox
+// file: file set to 'true' or 'false' (example use is linking to a pdf but still need validation)
+// form: name of the form to use (passes to setFormSubmitHandler)
+// jQuery.fn.setColorBox = function () {
+//   var button = $(this).attr('button');
+//   var width = $(this).attr('width');
+//   var height = $(this).attr('height');
+//   var action = $(this).attr('action');
+//   var file = $(this).attr('file');
+//   var form = $(this).attr('form');
+//   if (width == ''){width = 350}; // default width
+//   if (height == ''){height = 240}; // default height
+//   $(this).colorbox({
+//     transition:"none",
+//     speed:500,
+//     initialHeight:height,
+//     initialWidth:width,
+//     innerHeight:height,
+//     innerWidth:width,
+//     onOpen:function(){  },
+//     onLoad:function(){  },
+//     onComplete:function(){ 
+//       showForm(form); // select which form to use
+//       // Focus on first visible text box
+//       $("input:text:visible:first").focus();
+//       // Close colorbox on click
+//       $('.closeColorBox').live('click', function(){
+//         $.fn.colorbox.close();
+//       });
+//       // Add focus class for iE  
+//       $('input').focus(function() {
+//         $(this).addClass("focused");
+//       });
+//       $('input[type="radio"],input[type="checkbox"]').focus(function() {
+//         $(this).parent("label").addClass("active");
+//       });
+//       $('input').blur(function() {
+//         $(this).removeClass("focused");
+//       });
+//       $('input[type="radio"],input[type="checkbox"]').blur(function() {
+//         $(this).parent("label").removeClass("active");
+//       });
+//       $("#demographicButton").val(button); // sets the button text
+//       setFormValues(); // Populate form values (based on cookie data)
+//       submitAction(); // Set submit() action to the form on click
+//       setFormSubmitHandler(action, file, form);
+//       $("#spinner").ajaxElement(); // want to show the spinner when fetching the county list
+//       zipShowCountyList();
+//     },
+//     onCleanup:function(){  },
+//     onClosed:function(){  }
+//   });
+// }
+
+// Set events to the zip field
+// function zipShowCountyList() {
+//   $("#counties").change(function () {
+//     setCountyOptionsToValues();
+//     if (debug){log('DEBUG: counties selectoption changed')};
+//   });
+//   $("#ZipCode").blur(function () {
+//     handleZip();
+//   });
+//   $("#ZipCode").keyup(function () {
+//     handleZip();
+//   });
+// }
+
+
+// Submit Handler for the form
+// var formValidated;
+// function setFormSubmitHandler(action, file, formName) {
+//   // Validation Rules
+//   var container = $('.formErrorsDemo');
+//   $('#getQuoteForm').validate({
+//     errorLabelContainer: container,
+//     errorClass: "errortext",
+//     errorPlacement: function(error, element) {
+//       $( container ).append(error);
+//     }, 
+//     wrapper: "p",
+//     ignore: ".ignore",
+//     rules: {
+//       zip: {
+//         required: true
+//       },
+//       county: {required: true},
+//       coverage: {required: true}, 
+//       //onMedicare: { required: true },
+//       g0: {required: true},
+//       a0: {
+//         required: true,
+//         greaterThanMedigapAge : true
+//       }
+//     },
+//     messages: {
+//       zip: "Please provide a zip code",
+//       county: "Please select a county",
+//       coverage: "Please select a coverage year",
+//       //onMedicare: "Regence Medicare products are only available if you are Medicare eligible. <a href='/individual/medical/index.jsp'>Learn more</a> about our other plan offerings.",
+//       g0: "Please select a gender",
+//       a0: {
+//         required: "Please enter an age",
+//         greaterThanMedigapAge: "To qualify for Medigap coverage, you must be 65 or older. You indicated that you are not 65."
+//       }
+//     },
+//     submitHandler: function(form){
+//       showSpinner();
+//       $.ajax({
+//         statusCode: {
+//             404: function() {
+//               validatePlanValue("", "SYSTEM_ERROR");
+//             }
+//         },
+//         type: "POST",
+//         url: "/planCodeService.do",
+//         data: "zip=" + $("#ZipCode").val(),
+//         success: function( xmlDoc ){
+//           $(xmlDoc).find("plancode").each(function(){
+//             var planValue = $(this).find('value').text();
+//             var area    = $(this).find('area').text();
+//             var errorValue  = $(this).find('error').text();
+//             var age     = $("#age").val();
+//             var coverageYear = $("input[name='coverage']:checked").val();
+//             var countyValue = $("#counties").val();
+//             var serviceArea = $("#serviceArea").val();
+//             var coverageLoc = $("input[name='coverage']:checked").val();
+//             // send user according to year of coverage selected
+//             if (coverageYear == '2010'){
+//               CoverageYearCheck(formName, planValue, coverageYear);
+//               return false;
+//             }
+//             
+//             
+//             // TEMP FIX: Asotin county medicare products are provided by ID and not ASURIS 
+//             // on the individual application Asotin is provided by ASURIS
+//             // 99403, 99402, 99401
+//             var z = $("#ZipCode").val();
+//             if ( z == "99401" || z == "99402" || z == "99403" ) {
+//               planValue = 'WARBS';
+//               area = 'id';
+//             }
+// 
+//             // if (errorValue == "COUNTY_CHALLENGE") {
+//             //  planValue = "OR";
+//             //  errorValue = "";
+//             // }
+//             
+//             // Set state value
+//             if (area == 'clark_co'){
+//               $("#state").val('WA');
+//             } else {
+//               //if (planValue == 'WARBS'){planValue = 'WA';}
+//               $("#state").val(planValue);
+//             }
+//             
+//             // TEMP: Asotin County in WA - medigap is offered through Asuris
+//             if (formName == 'medigap' && countyValue == 'Asotin'){
+//               var eMessage = "This zip code is out of our service area."; //BCBSA cannot reference affiliates
+//               displayError(eMessage, ".formErrorsDemo");
+//               $.colorbox.resize();
+//               formValidated = false;
+//               return false;
+//             }
+//             
+//             validatePlanValue(planValue, errorValue); // validate planValue [is a post validation]
+//             
+//             if ( formValidated == false) {
+//               return false;
+//             }
+// 
+//             // Everything checks out, set our cookie
+//             setCookieString();
+//             
+//             // direct page
+//             partDonly(action); // This will redirect users to another page if only part D is offered for their region
+//             if ( formValidated == false) {
+//               return false;
+//             }else if ( file == "true"){
+//               // if we are linking to a file
+//               // replace PLANCODE in the link
+//               if (planValue == 'WARBS'){planValue = 'WA'}
+//               $.cookie("ZIPCODE", z, { path: '/', expires: 0 });
+//               action = action.replace(/PLANCODE/i, planValue);
+//               window.location = action;
+//               return false;
+//             } else {
+//               // set plancode cookie
+//               var cookie_data;
+//               if (serviceArea != 'default'){
+//                 cookie_data = serviceArea;
+//               } else {
+//                 cookie_data = planValue;
+//               }
+//               if(debug){log("Cookie data being passed to PLANCODE:\n"+cookie_data)}
+//               $.cookie("PLANCODE", ( cookie_data ), { path: '/', expires: 0 });
+//               $.cookie("ZIPCODE", z, { path: '/', expires: 0 });
+//               $.cookie("county", countyValue, { path: '/', expires: 0 });
+//               setCookieString();
+//               
+//               var url = "/medicare/"+coverageLoc+"?"+$("#getQuoteForm").serialize();
+//               window.location = url;
+//             }
+//             
+//           });
+//         }
+//       });
+//     }
+//   });
+// }
+// Show the main spinner for submitting form
+// function showSpinner() {
+//   $("#getQuoteForm").attr('style','visibility:hidden').before('<div class="spinner" style=""><h2>Loading...</h2></div>');
+//   $("#cboxTitle").attr('style','visibility:hidden');
+// }
 
 /***********************
 *
@@ -410,6 +731,104 @@ function checkAvail(dataModel) {
       }
     });
     
+  }
+}
+
+/***********
+*	@name:			setFormValues()
+*	@description:	Check if MedAdvantage and/or Medigap is offered, remove tabs if needed
+*
+*	@param: 		(object) 		dataModel Data Model for Value checking
+*	@returns: 		N/A
+*
+*	@TODO:			Refactor and make run on Data Model. Reduce Minification and reduce ID calls. Make more open and abstract.
+*
+***********/
+function setFormValues(dataModel) {
+  if( getCookie("demographics") ) {
+    var c = eval('(' + getCookie("demographics") + ')');
+    var gender = c['gender']; 
+    var coverage = c['coverage'];
+    var county = c['county'];
+    var serviceArea = c['serviceArea'];
+    $("#gender option[value='"+gender+"']").attr('selected', 'selected'); // Set the gender
+    $("#zipCode").val(c['zip']); // Set the zip
+    $("#age").val(c['age']); // Set the age
+    $("#serviceArea").val(c['serviceArea']); // Set the serviceArea
+    $("#offerMedigap").val(c['offerMedigap']); // Offer Medigap
+    $("#offerMedAdvantage").val(c['offerMedAdvantage']); // Offer MedAdvantage
+    if (c['onMedicare'] == "on"){$("#alreadyMedicare").attr('checked', true);} // Set alreadyMedicare
+    if (c['rememberMe'] == "on"){ $("#rememberMe").attr('checked', true); } // Set remember me
+    $("#coverage"+coverage).attr('checked', 'checked'); // Set coverage selected
+
+
+    // Create the list of Counties
+    var countyList = [];
+    var state = c['state'];
+
+    if(debug){
+      alert('state is returning: '+state);
+    }
+    if (typeof(state) == "undefined" || state == '') {
+      state=pCode;
+    }
+    if (typeof(state) == "undefined" || state == '') {
+      state=pCode;
+    }
+    countyList = $(this).getCountyList(state);
+    
+    var send_data = { 'collection':countyList,'selected':county };
+    createCountyList(send_data);
+    
+  } else if ( getCookie("ZIP") ) {
+    var zip = getCookie("ZIP");
+    $("#zipCode").val(zip); // Set the zip
+  } else { 
+    // do nothing 
+  }
+  
+  // Java reads the plancode cookie seperate (in this case plancode = planvalue)
+  if ( getCookie("PLANCODE")){
+    var planValue = getCookie("PLANCODE");
+    $("#planvalue").val(planValue); // Set the planValue
+  }
+  
+  // TODO: need a better solution here so the value of 0 doesn't show up in the age field
+  // Need to set default value so java doesn't blow up
+  if ( $("#age").attr('type') == 'hidden' ) { $("#age").val('0'); }
+}
+
+/***********
+*	@name:			setFormValues()
+*	@description:	Check if MedAdvantage and/or Medigap is offered, remove tabs if needed
+*
+*	@param: 		(string) 		planValue Plan Value is the Brand Info/Region
+*	@param: 		(string) 		errorValue Error Returned by something or other.
+*	@returns: 		N/A
+*
+*	@TODO:			Refactor and make run on Data Model. Reduce Minification and reduce ID calls. Abstract and Roll into approp
+*
+***********/
+// Validation for the Plan Value 
+// checks if ERROR, [empty], or ASURIS is being returned from planCodeService.do
+function validatePlanValue(planValue, errorValue) {
+  // validation rules
+  var eMessage;
+  if (errorValue == "POORLY_FORMATTED_ZIP") {
+    displayError("Please enter a 5 digit zip code.", ".formErrorsDemo");
+    formValidated = false;
+  }
+  else if (errorValue == "OUT_OF_AREA_ERROR" || errorValue == "WRONG_BRAND" || planValue == 'ASURIS' || planValue == 'AS') {
+    displayError("We do not offer Medicare coverage options to residents of the zip code you entered. You can find a Blue Cross and Blue Shield company near you at <a href='http://www.bcbs.com' target='_blank'>www.bcbs.com</a>", ".formErrorsDemo"); //BCBSA cannot reference affiliatesfs
+    formValidated = false;
+  }
+  else if ( errorValue == "SYSTEM_ERROR" || planValue == 'ERROR' || planValue == '' ) {
+    eMessage = "We're sorry, but there was a problem finding Medicare plans for you.  For information about our Medicare  plans, please contact us at 1-800-541-8981, (or TTY 711),  between 8 a.m. and 8 p.m. Monday through Friday.  Between October 15 through February 14 our telephone hours are 8 a.m. to 8 p.m., seven days a week.  Customer Service also has free language interpreter services available for non-English speakers.";
+    displayError(eMessage, ".formErrorsDemo");
+    formValidated = false;
+  } else {
+    removeErrors();
+    formValidated = true;
   }
 }
 
